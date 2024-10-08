eeec9ffdc796a879427778008fd85bdccd58f190
smivys <khanhnamxz@hotmail.com>
2024-10-08 22:26:40 +0200

    "Complete Jaccard function, now the program can compute Jaccard distance between sequences.

--- TP/__main__.py
+++ TP/__main__.py
@@ -1,13 +1,24 @@
 from TP.loading import load_directory
-from TP.kmers import stream_kmers, kmer2str
+from TP.kmers import stream_kmers, kmer2str, enumerate_kmer
+import os
 
 
 
 def jaccard(fileA, fileB, k):
-    j = 0
+    # j = 0
     # --- To complete ---
-    return j
+    index = stream_kmers(fileA, k)
+    intersect = 0
+    union = sum(index.values())
+    for kmer in enumerate_kmer(fileB, k):
+        if kmer in index and index[kmer] > 0:
+            intersect += 1
+            index[kmer] -= 1
+    else:
+        union += 1
 
+    return round(intersect/union, 3)
+    # return j
 
 
 if __name__ == "__main__":
@@ -16,13 +27,33 @@ if __name__ == "__main__":
     # Load all the files in a dictionary
     files = load_directory("data")
     k = 21
-    
+            
     print("Computing Jaccard similarity for all pairs of samples")
     filenames = list(files.keys())
+    matrix = [list(range(0, len(filenames)+1))]
+    for i in range(1, len(filenames)+1):
+        matrix.append(list(range(i, len(filenames)+1+i)))
+    
     for i in range(len(files)):
-        for j in range(i+1, len(files)):
-            
+        matrix[0][i+1] = filenames[i]
+        matrix[i+1][0] = filenames[i]
+        for j in range(len(files)):
             # --- Complete here ---
+            if i == j:
+                matrix[i+1][j+1] = 0
+            else:
+                J = jaccard(files[filenames[i]][0], files[filenames[j]][0], k)
+                print(filenames[i], filenames[j], J)
+                matrix[j+1][i+1] = J
+    
+    print('Distance matrix: \n',matrix)
 
-            j = jaccard(files[filenames[i]], files[filenames[j]], k)
-            print(filenames[i], filenames[j], j)
+ 
+    file_path = 'distance_matrix.txt'
+    if not os.path.exists(file_path):
+        with open(file_path, 'w') as file:
+            for row in matrix:
+                file.write(str(row))
+                file.write('\n')
+    else:
+        print(f"The file {file_path} already exists.")
--- TP/kmers.py
+++ TP/kmers.py
@@ -1,3 +1,26 @@
+from collections import defaultdict
+
+def encode_nucl(letter):
+    encoding = {'A': 0, 'C': 1, 'T': 2, 'G': 3}
+    return encoding[letter]
+
+def encode_kmer(seq, k):
+    kmer = 0
+    for letter in seq[0:k]:
+        kmer <<= 2
+        kmer += encode_nucl(letter)
+    return kmer
+
+def enumerate_kmer(seq, k):
+    kmer = encode_kmer(seq, k) #seq[0:k]
+    mask = (1 << (2*(k-1))) - 1
+    for i in range(len(seq) - k + 1):
+        yield kmer
+        kmer &= mask # kmer = kmer[1:] #
+        kmer <<= 2 # kmer.append(seq[i + k])
+        kmer |= encode_nucl(seq[i + k - 1])
+    yield kmer
+
 
 def kmer2str(val, k):
     """ Transform a kmer integer into a its string representation
@@ -14,7 +37,9 @@ def kmer2str(val, k):
     str_val.reverse()
     return "".join(str_val)
 
-
 def stream_kmers(text, k):
     # --- To complete ---
-    pass
+    res = defaultdict(int)
+    for i in range(len(text)-k+1):
+        res[encode_kmer(text[i:i+k], k)] += 1
+    return res
